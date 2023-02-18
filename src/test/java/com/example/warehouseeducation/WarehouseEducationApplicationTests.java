ff34bf6098ff2b2ca52914110a18f39d770467a3
Alex <Gav16071997@gmail.com>
2023-02-18 22:19:12 +0300

    Created warehouse-education

---
+++ src/test/java/com/example/warehouseeducation/WarehouseEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouseeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
