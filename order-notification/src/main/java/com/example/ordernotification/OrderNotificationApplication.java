cda2446511b1325e541717f6f1a128b5087746e1
Alex <Gav16071997@gmail.com>
2023-06-15 13:58:32 +0300

    Add order notification service

---
+++ order-notification/src/main/java/com/example/ordernotification/OrderNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNotificationApplication.class, args);
+    }
+
+}
---
+++ order-notification/src/main/java/com/example/ordernotification/service/EmailService.java
@@ -0,0 +1,13 @@
+package com.example.ordernotification.service;
+
+import jakarta.mail.MessagingException;
+
+import java.io.FileNotFoundException;
+
+public interface EmailService {
+
+    void sendSimpleEmail(String toAddress, String subject, String message);
+
+    void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment) throws MessagingException, FileNotFoundException;
+
+}
